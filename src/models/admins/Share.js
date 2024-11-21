9db54009d8c6bba6ebdc3ad458de7e5b64d92558
shumetey <shumetey12@gmail.com>
2024-11-21 17:03:59 +0300

    After solving the error merging

--- src/models/admins/Share.js
+++ src/models/admins/Share.js
@@ -7,6 +7,7 @@ class Share extends BaseModel {}
         id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true, allowNull: false},
         shareValue: { type: DataTypes.TEXT, allowNull: false, trim: true },
         description: { type: DataTypes.TEXT, allowNull: true, trim: true, defaultValue: null },
+        trash: { type: DataTypes.BOOLEAN, allowNull: false, trim: true, defaultValue: false }
     });
 
 export default Share;
