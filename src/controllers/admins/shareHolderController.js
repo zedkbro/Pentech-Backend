312fbbe34fc269d4ff237f175aeedb88083f56c7
shumetey <shumetey12@gmail.com>
2024-11-22 14:53:41 +0300

    After adding all recent components

--- src/controllers/admins/shareHolderController.js
+++ src/controllers/admins/shareHolderController.js
@@ -38,23 +38,23 @@ class ShareHolderController extends SuperController {
   
   async getAll(req, res) {  
     try {  
-        const { trash } = req.query;  
-        let result;  
-        if (trash === 'true') {  
-            result = await service.findAllSharePopulatedData(Admin, "usedData", { trash: true });
-            // result = await service.findAllSharePopulatedData(Share, "shareData", { trash: true });
-        } else {  
-            result = await service.findAllSharePopulatedData(Admin, "usedData", { trash: false });
-            // result = await service.findAllSharePopulatedData(Share, "shareData", { trash: false });
-        }   
         // const { trash } = req.query;  
-        // const filterOptions = { trash: trash === 'true' };  
-        // const includeOptions = [
-        //     { model: Admin, required: false },  // LEFT JOIN Admin
-        //     { model: Share, required: false }   // LEFT JOIN Share
-        // ];
-        // const result = await service.findAllSharePopulatedData(Admin, "usedData", { trash: true });
-        if (!result || result.length === 0) {  
+        // let result;  
+        // if (trash === 'true') {  
+        //     result = await service.findAllSharePopulatedData(Admin, "usedData", { trash: true });
+        //     // result = await service.findAllSharePopulatedData(Share, "shareData", { trash: true });
+        // } else {  
+        //     result = await service.findAllSharePopulatedData(Admin, "usedData", { trash: false });
+        //     // result = await service.findAllSharePopulatedData(Share, "shareData", { trash: false });
+        // }   
+        const { trash } = req.query;  
+        const filterOptions = { trash: trash === 'true' };  
+        const includeOptions = [
+            { model: Admin, required: false },  // LEFT JOIN Admin
+            { model: Share, required: false }   // LEFT JOIN Share
+        ];
+        const result = await service.findAllSharePopulatedData(includeOptions, "usedData", filterOptions);
+        if (!result) {  
             return ResponseHandler.sendUnSuccessResponse(res, 'No ShareHolder found!');  
         } else {  
             return ResponseHandler.sendSuccessResponse(res, result);  
--- src/models/admins/Admin.js
+++ src/models/admins/Admin.js
@@ -1,17 +1,24 @@
-import BaseModel from '../BaseModel.js';
-import { DataTypes } from 'sequelize';
+import BaseModel from '../BaseModel.js';  
+import { DataTypes } from 'sequelize';  
 
-class Admin extends BaseModel {}
+class Admin extends BaseModel {}  
 
-Admin.init({
-  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true, allowNull: false},
-  name: { type: DataTypes.STRING, allowNull: false, trim: true },
-  email: { type: DataTypes.STRING, allowNull: false, trim: true, unique: true, lowercase: true, validate: { notEmpty: true } },
-  password: { type: DataTypes.STRING, allowNull: false, trim: true, validate: { notEmpty: true } }, 
-  phoneNumber: { type: DataTypes.STRING, allowNull: false, trim: true, unique: true, validate: { notEmpty: true } },
-  role: { type: DataTypes.STRING, allowNull: false, trim: true, validate: { isIn: [['admin', 'shareholder']], notEmpty: true } },
-  avatar: { type: DataTypes.STRING, allowNull: true, trim: true, defaultValue: null },
-  trash: { type: DataTypes.BOOLEAN, allowNull: false, trim: true, defaultValue: false }
-});
+Admin.init({  
+  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true, allowNull: false },  
+  name: { type: DataTypes.STRING, allowNull: false, trim: true },  
+  email: { type: DataTypes.STRING, allowNull: false, trim: true, lowercase: true, validate: { notEmpty: true } },  
+  password: { type: DataTypes.STRING, allowNull: false, trim: true, validate: { notEmpty: true } },  
+  phoneNumber: { type: DataTypes.STRING, allowNull: false, trim: true, validate: { notEmpty: true } },  
+  role: { type: DataTypes.STRING, allowNull: false, trim: true, validate: { isIn: [['admin', 'shareholder']], notEmpty: true } },  
+  avatar: { type: DataTypes.STRING, allowNull: true, trim: true, defaultValue: null },  
+  trash: { type: DataTypes.BOOLEAN, allowNull: false, trim: true, defaultValue: false }  
+}, {  
+  indexes: [  
+    {  
+      unique: true,  
+      fields: ['email', 'phoneNumber']    
+    }  
+  ],  
+});  
 
 export default Admin;
