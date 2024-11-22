c12bfeefd0e85ee76e3a4a20e8b51f52898de077
shumetey <shumetey12@gmail.com>
2024-11-22 11:15:43 +0300

    After adding all recent components

--- src/models/admins/Admin.js
+++ src/models/admins/Admin.js
@@ -9,7 +9,7 @@ Admin.init({
   email: { type: DataTypes.STRING, allowNull: false, trim: true, unique: true, lowercase: true, validate: { notEmpty: true } },
   password: { type: DataTypes.STRING, allowNull: false, trim: true, validate: { notEmpty: true } }, 
   phoneNumber: { type: DataTypes.STRING, allowNull: false, trim: true, unique: true, validate: { notEmpty: true } },
-  role: { type: DataTypes.STRING, allowNull: false, trim: true, validate: { isIn: ['admin', 'staff', 'shareholder'], notEmpty: true } },
+  role: { type: DataTypes.STRING, allowNull: false, trim: true, validate: { isIn: ['admin', 'shareholder'], notEmpty: true } },
   avatar: { type: DataTypes.STRING, allowNull: true, trim: true, defaultValue: null },
   trash: { type: DataTypes.BOOLEAN, allowNull: false, trim: true, defaultValue: false }
 });
